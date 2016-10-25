b797229d902edc994c4fc3610f63e10e31cbfce3
Andrew VanVlack <A.VanVlack@Gmail.com>
2016-10-25 04:01:43 -0700

    fin

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
-var express = require('express');
-var morgan = require('morgan');
-var app = express();
+let express = require('express'),
+    morgan = require('morgan'),
+    path = require('path'),
+    app = express();
 
 app.use(morgan('dev'));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+
+//Date from unix route
+app.get('/:date(\\d+)/', (req, res) => {
+  let date = new Date(parseInt(req.params.date * 1000, 10));
+  payload = jsonDate(date);
+  res.json(payload);
+});
+
+//Date from natural languae route
+app.get('/:date', (req, res) => {
+  let date = new Date(req.params.date);
+  payload = jsonDate(date);
+  res.json(payload)
+});
+
+//index/example of use route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+//turns Date into payload object
+function jsonDate(date){
+  let dateData = {};
+  if(date instanceof Date && isFinite(date)){
+    dateData.unix = Math.floor(date.getTime() / 1000);
+    let month = date.toLocaleString("en-us", { month: "long" });
+    dateData.natural = month + " " + date.getUTCDate() + ", " + date.getUTCFullYear()
+  }
+  else {
+    dateData = {
+      unix: null,
+      natural: null
+    }
+  }
+  return dateData
+}
